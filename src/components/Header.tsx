5a0cbfa1f2ab0915a894614e847d9b71e143db03
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-07-12 20:06:30 -0300

    style: updated header style when route not is home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,21 @@
 import { Container } from "./Container";
+import { NavLink, useLocation } from "react-router-dom";
 import Logo from "../assets/Logo.svg";
 
 export function Header() {
+  const { pathname } = useLocation();
+
   return (
-    <header>
+    <header
+      className={`h-24 flex justify-center items-center ${
+        pathname !== "/" && "bg-white border-b border-gray-100"
+      }`}
+    >
       <Container>
         <div className="flex justify-between items-center py-8">
-          <img src={Logo} alt="" />
+          <NavLink to="/">
+            <img src={Logo} alt="" />
+          </NavLink>
           <button className="bg-blue-100 w-[174px] h-12 rounded-[10px] text-blue-500 font-medium hover:bg-blue-500 hover:text-white transition-colors">
             Acesso restrito
           </button>
