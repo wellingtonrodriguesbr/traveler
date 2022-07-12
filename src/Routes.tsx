f6600cd4674a2b1e46910d41bbd65431007d176a
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-07-12 20:04:26 -0300

    chore: rename

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
+import { AllCitys } from "./pages/AllCitys";
 import { Home } from "./pages/Home";
 
 export function Router() {
@@ -7,6 +8,7 @@ export function Router() {
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/allcitys" element={<AllCitys />} />
       </Route>
     </Routes>
   );
