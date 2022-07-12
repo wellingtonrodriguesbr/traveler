8c4a9461daf7e1b2132efab1ed04877f7dd2ace3
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-07-12 20:05:02 -0300

    style: update max width

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,5 +5,5 @@ interface ContainerProps {
 }
 
 export function Container({ children }: ContainerProps) {
-  return <div className="w-full max-w-[1120px] mx-auto px-4">{children}</div>;
+  return <div className="w-full max-w-[1440px] mx-auto px-4">{children}</div>;
 }
