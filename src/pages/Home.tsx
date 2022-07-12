8e725da42f22eca16b62c3d58e41d26df3b4654f
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-07-12 18:59:44 -0300

    feat: finished home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,26 +4,26 @@ import { Container } from "../components/Container";
 export function Home() {
   return (
     <Container>
-      <div className="flex justify-between h-screen overflow-y-hidden">
-        <div className="max-w-xs mt-[138px]">
-          <h1 className="font-[barlow] font-semibold text-[5rem] text-blue-700 leading-[74px]">
+      <div className="flex flex-col xl:flex-row justify-between h-full xl:max-h-screen xl:pb-0 pb-96 overflow-hidden">
+        <div className="w-full mt-12 xl:mt-[108px] flex flex-col justify-center items-center xl:items-start xl:justify-start">
+          <h1 className="font-[barlow] font-semibold text-5xl xl:text-[5rem] text-blue-700 xl:leading-[74px] text-center xl:text-left">
             Viva uma grande aventura
           </h1>
-          <p className="mt-10 text-gray-500 text-xl leading-relaxed">
+          <p className="mt-10 text-gray-500 text-xl leading-relaxed text-center xl:text-left">
             Descubra locais incríveis para se visitar em cidades maravilhosas de
             Santa Catarina.
           </p>
-          <button className="mt-12 w-[329px] h-[72px] bg-orange-500 text-white rounded-[10px] text-lg hover:brightness-90 transition-colors">
+          <button className="mt-12 px-8 xl:w-[329px] h-[72px] bg-orange-500 text-white rounded-[10px] text-lg hover:brightness-90 transition-colors">
             Descobrir todos os lugares
           </button>
         </div>
-        <div className="flex gap-8">
-          <div className="flex flex-col gap-8">
+        <div className="flex flex-col xl:flex-row justify-center items-center gap-8 mt-12 xl:mt-96">
+          <div className="flex overflow-auto w-full flex-row xl:flex-col gap-8 mt-4">
             <CardCity imgSrc="https://source.unsplash.com/random/?city" />
             <CardCity imgSrc="https://source.unsplash.com/random/?street" />
             <CardCity imgSrc="https://source.unsplash.com/random/?brazil" />
           </div>
-          <div className="flex flex-col gap-8 mt-14">
+          <div className="flex overflow-auto w-full flex-row xl:flex-col gap-8 mt-4 xl:mt-[92px]">
             <CardCity imgSrc="https://source.unsplash.com/random/?brazil" />
             <CardCity imgSrc="https://source.unsplash.com/random/?canada" />
             <CardCity imgSrc="https://source.unsplash.com/random/?america" />
