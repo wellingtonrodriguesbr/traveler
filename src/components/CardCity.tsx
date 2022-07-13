39ef651f8e07a69d30b555ce871d49c6e6efeb58
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-07-13 19:57:32 -0300

    style: set width and height image 100%

--- src/components/CardCity.tsx
+++ src/components/CardCity.tsx
@@ -6,7 +6,11 @@ export function CardCity({ imgSrc }: CardCityProps) {
   return (
     <div className="w-full min-w-[254px] xl:w-[254px] h-[316px] bg-white rounded-[20px] flex flex-col border border-gray-100">
       <div className="h-[210px] w-full overflow-hidden object-cover">
-        <img className="rounded-t-[20px]" src={imgSrc} alt="" />
+        <img
+          className="object-cover h-full w-full rounded-t-[20px]"
+          src={imgSrc}
+          alt=""
+        />
       </div>
       <div className="m-7">
         <h4 className="text-blue-700 text-xl font-semibold font-[barlow]">
