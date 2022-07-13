3f9c9055e6a354854660f92f572f6934a6023c46
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-07-13 19:57:46 -0300

    feat: create route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { AllCitys } from "./pages/AllCitys";
+import { City } from "./pages/City";
 import { Home } from "./pages/Home";
 
 export function Router() {
@@ -8,7 +9,8 @@ export function Router() {
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="allcitys" element={<AllCitys />} />
+        <Route path="/allcitys" element={<AllCitys />} />
+        <Route path="/city" element={<City />} />
       </Route>
     </Routes>
   );
