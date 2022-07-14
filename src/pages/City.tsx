6a2d546dec736d97fb5dcffcc51a58569c4d9723
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-07-14 05:34:30 -0300

    feat: WIP City Page

---
+++ src/pages/City.tsx
@@ -0,0 +1,13 @@
+import { Container } from "../components/Container";
+
+export function City() {
+  return (
+    <div>
+      <div></div>
+      <Container>
+        <div></div>
+        <div></div>
+      </Container>
+    </div>
+  );
+}
